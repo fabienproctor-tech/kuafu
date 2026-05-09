36c0a141a25fe7f817d7f34ba57f705a9282505f
Fabien Proctor <fabienproctor@gmail.com>
2026-05-09 18:28:26 +0100

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://www.kuafu.co.uk";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/search`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/route-generator`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/jobs`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/destinations`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/guides`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+  ];
+}
